deeec7f479517ab0047277e47e0e5cd6aababb76
Niyi Ogungbemi <niyiment@gmail.com>
2025-07-31 12:26:50 +0100

    Initial commit

---
+++ src/main/java/com/niyiment/approval_flow/ApprovalFlowApplication.java
@@ -0,0 +1,13 @@
+package com.niyiment.approval_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprovalFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niyiment/approval_flow/ApprovalFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niyiment.approval_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprovalFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
